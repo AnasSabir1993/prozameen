7495b9cada1575b9a2f5deb012e5f9dc36ae3799
bexperts <34183571+bexperts@users.noreply.github.com>
2017-12-04 11:09:12 +0500

    Add files via upload

---
+++ App_Code/AutoService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.UI.WebControls;
+
+
+
+/// <summary>
+/// Summary description for AutoService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+[System.Web.Script.Services.ScriptService]
+public class AutoService : System.Web.Services.WebService
+{
+
+    public AutoService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public List<string> GetAreaNames(int ID)
+    {
+        DbConnection db = new DbConnection();
+
+        SqlConnection connection = new SqlConnection(db.connectionString);
+        connection.Open();
+
+        ID = db.CityID;
+        
+
+        SqlCommand cmd = new SqlCommand("spGetmatchingAreasNames", connection);
+        cmd.CommandType = CommandType.StoredProcedure;
+
+
+        SqlParameter parameter = new SqlParameter("@locID", ID);
+        cmd.Parameters.Add(parameter);
+
+        List<string> Area_Names = new List<string>();
+
+        SqlDataReader dr = cmd.ExecuteReader();
+        while (dr.Read())
+        {
+            if (dr.HasRows)
+            {
+                Area_Names.Add(dr["Area_Names"].ToString());
+            }
+        }
+       
+        return Area_Names;
+    }
+
+
+   }
---
+++ App_Code/DbConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+/// <summary>
+/// Summary description for SqlConnection
+/// </summary>
+public class DbConnection
+{
+	public DbConnection()
+	{
+        connectionString = "Data Source=JP-PC;Initial Catalog=ProZameen;Integrated Security=True";        //Please change this	
+	}
+     public string connectionString
+    {
+        get; // A new way to create getter/setter
+        set;
+    }
+
+     public int CityID
+     {
+         get;
+         set;
+     }
+
+}
